8f61bf1833ca6f568ef1d1ae91f1232d38b0ea8f
OmarNoman <omarrashadnoman@gmail.com>
2025-09-30 21:52:28 +1000

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
 import sqlite3
 import pytest
 from python_login import register, login, create_item, read_items, update_item, delete_item
