02c78385f97301a502a0813ff7a4c59d1b6a4356
OmarNoman <omarrashadnoman@gmail.com>
2025-10-02 21:23:56 +1000

    Update app.py

--- python_login_webapp/app.py
+++ python_login_webapp/app.py
@@ -97,4 +97,8 @@ def logout():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    if os.environ.get("ENV") == "production":
+        app.run(host="0.0.0.0", port=5000, debug=False)
+    else:
+        app.run(host="0.0.0.0", port=5000, debug=True)
